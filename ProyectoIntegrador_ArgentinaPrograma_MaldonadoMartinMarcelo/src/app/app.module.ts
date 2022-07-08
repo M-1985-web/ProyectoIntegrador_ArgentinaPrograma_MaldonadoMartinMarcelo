e9f0c60e697aa715023c3d6a05a3accd5ba87664
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-07-08 20:46:50 -0300

    cree componentes de header, logo, redes sociales, instale boostrap desde npm

--- ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/src/app/app.module.ts
+++ ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProgrComponent } from './components/logo-arg-progr/logo-arg-progr.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgrComponent,
+    RedesSocialesComponent,
+    BannerComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
