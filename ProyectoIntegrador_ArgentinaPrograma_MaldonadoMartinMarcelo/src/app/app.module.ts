72f6630d1961f251dfd4716432b2d20b3ab0b281
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-07-09 18:05:15 -0300

    cree componente sobreMi, me falta ajustar el responsive

--- ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/src/app/app.module.ts
+++ ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgrComponent } from './components/logo-arg-progr/logo-arg-progr.component';
 import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoArgProgrComponent,
     RedesSocialesComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
 
   imports: [
