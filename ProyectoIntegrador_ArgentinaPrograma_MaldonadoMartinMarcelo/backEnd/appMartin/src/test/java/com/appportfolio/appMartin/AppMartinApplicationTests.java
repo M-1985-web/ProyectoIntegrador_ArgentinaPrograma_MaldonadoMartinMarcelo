346d1e5ca148a1c0dd3be132877ff7967ed19820
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-07-11 17:54:15 -0300

    cree carpeta backend, cree proyecto Spring Initializr

---
+++ ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/backEnd/appMartin/src/test/java/com/appportfolio/appMartin/AppMartinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appportfolio.appMartin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMartinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
