f1a4c196db69a3c8f4926231802ae10bd68e949b
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-07-10 22:43:25 -0300

    elimine componente RS, trabaje en la parte responsive, casi listo el front

--- ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/src/app/app.module.ts
+++ ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgrComponent } from './components/logo-arg-progr/logo-arg-progr.component';
-import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -21,7 +20,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoArgProgrComponent,
-    RedesSocialesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
