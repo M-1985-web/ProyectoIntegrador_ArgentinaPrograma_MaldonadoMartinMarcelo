9ef92d9d05c1c81d3213395eb733fc95825cff3c
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-07-11 16:11:43 -0300

    carpeta frontend

---
+++ ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/FrontEnd/src/app/components/logo-arg-progr/logo-arg-progr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-arg-progr',
+  templateUrl: './logo-arg-progr.component.html',
+  styleUrls: ['./logo-arg-progr.component.css']
+})
+export class LogoArgProgrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
