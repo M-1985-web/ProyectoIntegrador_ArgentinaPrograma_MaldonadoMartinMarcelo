44f999dfbbd503062ebca09beae68b8b4186a9eb
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-07-13 22:05:05 -0300

    agregue carpeta backend y configure la base de datos

---
+++ ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/BackEnd/src/main/java/com/portfolioapp/MartinMM/Interface/IPersonaService.java
@@ -0,0 +1,20 @@
+package com.portfolioapp.MartinMM.Interface;
+
+import com.portfolioapp.MartinMM.Entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+    //traer una lista de persona
+    public List<Persona> getPersona();
+    
+    //guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+
+    //eliminar un objeto, usuario pero lo buscamos por id
+    public void deletePersona(Long id);
+
+    //buscar una persona por ID
+    public Persona findPersona(Long id);
+
+}
