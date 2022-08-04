52f25f3d33d774a036d989a6fc0d8dea7191f5d6
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-08-03 22:44:19 -0300

    modifique el json, archivo pom.xml, main y personaController del backend

--- ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/backEnd/mmm/src/main/java/com/apportfolio/mmm/Controller/PersonaController.java
+++ ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/backEnd/mmm/src/main/java/com/apportfolio/mmm/Controller/PersonaController.java
@@ -4,7 +4,7 @@ import com.apportfolio.mmm.Entity.Persona;
 import com.apportfolio.mmm.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
