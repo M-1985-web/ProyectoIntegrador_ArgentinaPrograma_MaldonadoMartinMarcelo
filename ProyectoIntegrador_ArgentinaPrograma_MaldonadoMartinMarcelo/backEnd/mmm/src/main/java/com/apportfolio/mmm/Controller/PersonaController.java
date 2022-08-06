6a763b10501cfdaf8cd4717ed49612e8f6a0fac7
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-08-06 16:44:56 -0300

    seleccione colores para componente login

--- ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/backEnd/mmm/src/main/java/com/apportfolio/mmm/Controller/PersonaController.java
+++ ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/backEnd/mmm/src/main/java/com/apportfolio/mmm/Controller/PersonaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -63,7 +63,7 @@ public class PersonaController {
     
     @GetMapping("/personas/traer/perfil")
     public Persona findPersona() {
-	return ipersonaService.findPersona((long)1);
+	return ipersonaService.findPersona((long)3);
     }
 
     
--- ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/src/app/components/logo-arg-progr/logo-arg-progr.component.ts
+++ ProyectoIntegrador_ArgentinaPrograma_MaldonadoMartinMarcelo/src/app/components/logo-arg-progr/logo-arg-progr.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 })
 export class LogoArgProgrComponent implements OnInit {
 
+  //aca le inyectamos servicio de ruteo
   constructor(private router:Router) { }
 
   ngOnInit(): void {
